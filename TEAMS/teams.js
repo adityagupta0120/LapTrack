ba6283ea4ecf23bfe3016d77ed596fd3b3e9d999
adityagupta0120 <neerajguptashivpurimp@gmail.com>
2026-03-30 19:07:59 +0530

    base code done for all pages

--- TEAMS/teams.js
+++ TEAMS/teams.js
@@ -1,12 +1,45 @@
-const container = document.getElementById("team-container");
+// const container = document.getElementById("team-container");
+// const searchInput = document.getElementById("searchInput");
+// let allTeams = [];
+// function displayTeams(teams) {
+//   container.innerHTML = "";
+//   teams.forEach(team => {
+//     const card = `
+//       <div class="card">
+//         <h2>${team.name}</h2>
+//         <p>${team.nationality}</p>
+
+//         <a href="${team.url}" target="_blank">Know More</a>
+//       </div>
+//     `;
+//     container.innerHTML += card;
+//   });
+// }
+// fetch('https://api.jolpi.ca/ergast/f1/constructors?limit=200')
+//   .then(res => res.json())
+//   .then(data => {
+//     allTeams = data.MRData.ConstructorTable.Constructors;
+//     displayTeams(allTeams);
+//   });
+// searchInput.addEventListener("input", () => {
+//   const value = searchInput.value.toLowerCase();
+//   const filtered = allTeams.filter(team =>
+//     team.name.toLowerCase().includes(value)
+//   );
+//   if (filtered.length === 0) {
+//     container.innerHTML = "<p style='color:white; text-align: center; background: rgba(0,0,0,0.6); padding: 20px; border-radius: 8px; height: 100px; line-height: 100px; width: 300px; font-size: 35px;'>No teams found.</p>";
+//     return;
+//   }
+//   displayTeams(filtered);
+// });
+
+
+const cont = document.getElementById("team-container");
 const searchInput = document.getElementById("searchInput");
-
-let allTeams = [];
-function displayTeams(teams) {
-  container.innerHTML = "";
-
+let all = [];
+function display(teams) {
+  cont.innerHTML = "";
   teams.forEach(team => {
-
     const card = `
       <div class="card">
         <h2>${team.name}</h2>
@@ -15,32 +48,27 @@ function displayTeams(teams) {
         <a href="${team.url}" target="_blank">Know More</a>
       </div>
     `;
-
-    container.innerHTML += card;
+    cont.innerHTML += card;
   });
 }
-
 fetch('https://api.jolpi.ca/ergast/f1/constructors?limit=200')
   .then(res => res.json())
   .then(data => {
-    allTeams = data.MRData.ConstructorTable.Constructors;
-    displayTeams(allTeams);
+    all = data.MRData.ConstructorTable.Constructors;
+    display(all);
   });
-
 searchInput.addEventListener("input", () => {
   const value = searchInput.value.toLowerCase();
-
-  const filtered = allTeams.filter(team =>
+  const filtered = all.filter(team =>
     team.name.toLowerCase().includes(value)
   );
-
   if (filtered.length === 0) {
-    container.innerHTML = "<p style='color:white; text-align: center; background: rgba(0,0,0,0.6); padding: 20px; border-radius: 8px; height: 100px; line-height: 100px; width: 300px; font-size: 35px;'>No teams found.</p>";
+    cont.innerHTML = "<p style='color:white; text-align: center; background: rgba(0,0,0,0.6); padding: 20px; border-radius: 8px; height: 100px; line-height: 100px; width: 300px; font-size: 35px;'>No teams found.</p>";
     return;
   }
-
-  displayTeams(filtered);
+  display(filtered);
 });
+
 
 
 
--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-
 function playSound() {
     const sound = document.getElementById("f1Sound");
     sound.currentTime = 0;
     sound.play();
 }
-
-
